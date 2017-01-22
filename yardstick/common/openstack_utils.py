6ea19219acae2e72fef70a1e361fc3d53f13e224
JingLu5 <lvjing5@huawei.com>
2017-01-22 10:33:51 +0000

    Add nova utilities
    Signed-off-by: JingLu5 <lvjing5@huawei.com>

--- yardstick/common/openstack_utils.py
+++ yardstick/common/openstack_utils.py
@@ -8,17 +8,24 @@
 ##############################################################################
 
 from __future__ import absolute_import
+
 import os
+import time
 import logging
 
 from keystoneauth1 import loading
 from keystoneauth1 import session
+from novaclient import client as novaclient
 
 log = logging.getLogger(__name__)
 
 DEFAULT_HEAT_API_VERSION = '1'
+DEFAULT_NOVA_API_VERSION = '2'
 
 
+# *********************************************
+#   CREDENTIALS
+# *********************************************
 def get_credentials():
     """Returns a creds dictionary filled with parsed from env"""
     creds = {}
@@ -89,9 +96,239 @@ def get_endpoint(service_type, endpoint_type='publicURL'):
                                       endpoint_type=endpoint_type)
 
 
+# *********************************************
+#   CLIENTS
+# *********************************************
 def get_heat_api_version():
     api_version = os.getenv('HEAT_API_VERSION')
     if api_version is not None:
         log.info("HEAT_API_VERSION is set in env as '%s'", api_version)
         return api_version
     return DEFAULT_HEAT_API_VERSION
+
+
+def get_nova_client_version():
+    api_version = os.getenv('OS_COMPUTE_API_VERSION')
+    if api_version is not None:
+        log.info("OS_COMPUTE_API_VERSION is set in env as '%s'", api_version)
+        return api_version
+    return DEFAULT_NOVA_API_VERSION
+
+
+def get_nova_client():
+    sess = get_session()
+    return novaclient.Client(get_nova_client_version(), session=sess)
+
+
+# *********************************************
+#   NOVA
+# *********************************************
+def get_instance_status(nova_client, instance):
+    try:
+        instance = nova_client.servers.get(instance.id)
+        return instance.status
+    except Exception, e:
+        log.error("Error [get_instance_status(nova_client)]: %s" % e)
+        return None
+
+
+def get_instance_by_name(nova_client, instance_name):
+    try:
+        instance = nova_client.servers.find(name=instance_name)
+        return instance
+    except Exception, e:
+        log.error("Error [get_instance_by_name(nova_client, '%s')]: %s"
+                  % (instance_name, e))
+        return None
+
+
+def get_aggregates(nova_client):
+    try:
+        aggregates = nova_client.aggregates.list()
+        return aggregates
+    except Exception, e:
+        log.error("Error [get_aggregates(nova_client)]: %s" % e)
+        return None
+
+
+def get_availability_zones(nova_client):
+    try:
+        availability_zones = nova_client.availability_zones.list()
+        return availability_zones
+    except Exception, e:
+        log.error("Error [get_availability_zones(nova_client)]: %s" % e)
+        return None
+
+
+def get_availability_zone_names(nova_client):
+    try:
+        az_names = [az.zoneName for az in get_availability_zones(nova_client)]
+        return az_names
+    except Exception, e:
+        log.error("Error [get_availability_zone_names(nova_client)]:"
+                  " %s" % e)
+        return None
+
+
+def create_aggregate(nova_client, aggregate_name, av_zone):
+    try:
+        nova_client.aggregates.create(aggregate_name, av_zone)
+        return True
+    except Exception, e:
+        log.error("Error [create_aggregate(nova_client, %s, %s)]: %s"
+                  % (aggregate_name, av_zone, e))
+        return None
+
+
+def get_aggregate_id(nova_client, aggregate_name):
+    try:
+        aggregates = get_aggregates(nova_client)
+        _id = [ag.id for ag in aggregates if ag.name == aggregate_name][0]
+        return _id
+    except Exception, e:
+        log.error("Error [get_aggregate_id(nova_client, %s)]:"
+                  " %s" % (aggregate_name, e))
+        return None
+
+
+def add_host_to_aggregate(nova_client, aggregate_name, compute_host):
+    try:
+        aggregate_id = get_aggregate_id(nova_client, aggregate_name)
+        nova_client.aggregates.add_host(aggregate_id, compute_host)
+        return True
+    except Exception, e:
+        log.error("Error [add_host_to_aggregate(nova_client, %s, %s)]: %s"
+                  % (aggregate_name, compute_host, e))
+        return None
+
+
+def create_aggregate_with_host(
+        nova_client, aggregate_name, av_zone, compute_host):
+    try:
+        create_aggregate(nova_client, aggregate_name, av_zone)
+        add_host_to_aggregate(nova_client, aggregate_name, compute_host)
+        return True
+    except Exception, e:
+        log.error("Error [create_aggregate_with_host("
+                  "nova_client, %s, %s, %s)]: %s"
+                  % (aggregate_name, av_zone, compute_host, e))
+        return None
+
+
+def create_instance(flavor_name,
+                    image_id,
+                    network_id,
+                    instance_name="instance-vm",
+                    confdrive=True,
+                    userdata=None,
+                    av_zone='',
+                    fixed_ip=None,
+                    files=None):
+    nova_client = get_nova_client()
+    try:
+        flavor = nova_client.flavors.find(name=flavor_name)
+    except:
+        flavors = nova_client.flavors.list()
+        log.error("Error: Flavor '%s' not found. Available flavors are: "
+                  "\n%s" % (flavor_name, flavors))
+        return None
+    if fixed_ip is not None:
+        nics = {"net-id": network_id, "v4-fixed-ip": fixed_ip}
+    else:
+        nics = {"net-id": network_id}
+    if userdata is None:
+        instance = nova_client.servers.create(
+            name=instance_name,
+            flavor=flavor,
+            image=image_id,
+            nics=[nics],
+            availability_zone=av_zone,
+            files=files
+        )
+    else:
+        instance = nova_client.servers.create(
+            name=instance_name,
+            flavor=flavor,
+            image=image_id,
+            nics=[nics],
+            config_drive=confdrive,
+            userdata=userdata,
+            availability_zone=av_zone,
+            files=files
+        )
+    return instance
+
+
+def create_instance_and_wait_for_active(flavor_name,
+                                        image_id,
+                                        network_id,
+                                        instance_name="instance-vm",
+                                        config_drive=False,
+                                        userdata="",
+                                        av_zone='',
+                                        fixed_ip=None,
+                                        files=None):
+    SLEEP = 3
+    VM_BOOT_TIMEOUT = 180
+    nova_client = get_nova_client()
+    instance = create_instance(flavor_name,
+                               image_id,
+                               network_id,
+                               instance_name,
+                               config_drive,
+                               userdata,
+                               av_zone=av_zone,
+                               fixed_ip=fixed_ip,
+                               files=files)
+    count = VM_BOOT_TIMEOUT / SLEEP
+    for n in range(count, -1, -1):
+        status = get_instance_status(nova_client, instance)
+        if status.lower() == "active":
+            return instance
+        elif status.lower() == "error":
+            log.error("The instance %s went to ERROR status."
+                      % instance_name)
+            return None
+        time.sleep(SLEEP)
+    log.error("Timeout booting the instance %s." % instance_name)
+    return None
+
+
+def delete_instance(nova_client, instance_id):
+    try:
+        nova_client.servers.force_delete(instance_id)
+        return True
+    except Exception, e:
+        log.error("Error [delete_instance(nova_client, '%s')]: %s"
+                  % (instance_id, e))
+        return False
+
+
+def remove_host_from_aggregate(nova_client, aggregate_name, compute_host):
+    try:
+        aggregate_id = get_aggregate_id(nova_client, aggregate_name)
+        nova_client.aggregates.remove_host(aggregate_id, compute_host)
+        return True
+    except Exception, e:
+        log.error("Error [remove_host_from_aggregate(nova_client, %s, %s)]:"
+                  " %s" % (aggregate_name, compute_host, e))
+        return False
+
+
+def remove_hosts_from_aggregate(nova_client, aggregate_name):
+    aggregate_id = get_aggregate_id(nova_client, aggregate_name)
+    hosts = nova_client.aggregates.get(aggregate_id).hosts
+    assert(
+        all(remove_host_from_aggregate(nova_client, aggregate_name, host)
+            for host in hosts))
+
+
+def delete_aggregate(nova_client, aggregate_name):
+    try:
+        remove_hosts_from_aggregate(nova_client, aggregate_name)
+        nova_client.aggregates.delete(aggregate_name)
+        return True
+    except Exception, e:
+        log.error("Error [delete_aggregate(nova_client, %s)]: %s"
+                  % (aggregate_name, e))
+        return False
