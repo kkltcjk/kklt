29447b2483460878622a0b301834ee15576737cd
JingLu5 <lvjing5@huawei.com>
2017-02-10 01:51:54 +0000

    change openstack cli to nova cli
    Signed-off-by: JingLu5 <lvjing5@huawei.com>

--- yardstick/benchmark/scenarios/compute/affinity-check.bash
+++ yardstick/benchmark/scenarios/compute/affinity-check.bash
@@ -1,2 +1,4 @@
-vm=$(openstack server list | grep $1 | awk '{print $4}')
-openstack server show $vm | grep OS-EXT-SRV-ATTR:host | awk '{print $4}'
+vm=$(nova list | grep $1 | awk '{print $4}')
+# vm=$(openstack server list | grep $1 | awk '{print $4}')
+nova show $vm | grep OS-EXT-SRV-ATTR:host | awk '{print $4}'
+# openstack server show $vm | grep OS-EXT-SRV-ATTR:host | awk '{print $4}'
